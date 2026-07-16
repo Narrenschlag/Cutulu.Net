4bb7569fab67ef911b539e5cf5ba4526a22e51e2
Maximilian Schecklmann <narrenschlag@gmail.com>
2026-07-16 18:51:45 +0200

    Update BinaryEncoder.cs

--- Core/Binary/Encoding/BinaryEncoder.cs
+++ Core/Binary/Encoding/BinaryEncoder.cs
@@ -27,4 +27,44 @@ public abstract class BinaryEncoder
     public abstract void Encode(BinaryWriter writer, Type type, object value);
 
     public abstract object Decode(BinaryReader reader, Type type);
+
+    public static SwapbackArray<EncoderMeta> EncodeMeta(BinaryWriter writer, params MetaEntry[] entries)
+    {
+        SwapbackArray<EncoderMeta> meta = new(entries.Size());
+
+        if (entries.NotEmpty())
+        {
+            LocalEncoder encoder = new();
+            long start;
+
+            foreach (var (name, value) in entries)
+            {
+                start = encoder.Position;
+                encoder.Write(value);
+                meta.Add(new(name, encoder.Position - start));
+            }
+
+            writer.Encode(meta);
+            writer.Write(encoder.GetBuffer());
+        }
+
+        return meta;
+    }
+
+    public static void DecodeMeta(BinaryReader reader, Action<string, byte[]> switchStatement)
+    {
+        var meta = reader.Decode<SwapbackArray<EncoderMeta>>();
+
+        byte[] buffer;
+
+        foreach (var m in meta)
+        {
+            buffer = reader.ReadBytes(m.Length);
+            if (buffer.IsEmpty()) continue;
+
+            switchStatement?.Invoke(m.ParamName, buffer);
+        }
+    }
+
+    public record struct MetaEntry(string ParamName, object Value);
 }
